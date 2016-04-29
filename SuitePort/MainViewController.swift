1537d9d7c51c3afd9e2b00d885e8e0cea7d78f63
Igor Eydman <igoreydman@gmail.com>
2016-04-29 12:53:17 -0400

    Set up conversation logic

--- SuitePort/MainViewController.swift
+++ SuitePort/MainViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 import Foundation
+import AVFoundation
 import SpeechKit
 
-class MainViewController: UIViewController, SKTransactionDelegate {
+class MainViewController: UIViewController, SKTransactionDelegate, AVSpeechSynthesizerDelegate {
     
     let locatonList = ["moon", "earth", "mars"]
     var myLocation :String?
+    
+    // Text to speech setup
+    let speechSynthesizer = AVSpeechSynthesizer()
+    var speechVoice : AVSpeechSynthesisVoice?
     
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var textFound: UILabel!
+    
+    @IBAction func talkPressed(sender: AnyObject) {
+        let utterance = "This is a talking test, do you hear me?"
+        sayThis(utterance)
+    }
     
     @IBAction func recordTapped(sender: AnyObject) {
         recordButton.setTitle("Listening", forState: .Normal)
@@ -32,35 +42,106 @@ class MainViewController: UIViewController, SKTransactionDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        // Web setup
         let url = NSURL(string: "http://suiteport.mybluemix.net/")
         let request = NSURLRequest(URL: url!)
         
-        webView.loadRequest(request)
         
+        webView.addSubview(textLabel)
+        webView.addSubview(textFound)
+        webView.addSubview(recordButton)
+        webView.loadRequest(request)
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        // Text to speech set up cont'd
+        
+        speechSynthesizer.delegate = self
+        
+        let voices = AVSpeechSynthesisVoice.speechVoices()
+        for voice in voices {
+            if "en-AU" == voice.language {
+                self.speechVoice = voice
+                print("\(voice) language checked")
+                break;
+            }
+        }
+    }
+    
     func transaction(transaction: SKTransaction!, didReceiveRecognition recognition: SKRecognition!) {
         //var result = recognition.text
         textLabel.text = recognition.text.lowercaseString
         
-        for location in locatonList {
-            if (textLabel.text!.rangeOfString("\(location)") != nil) {
-                print("\(location)")
-                textFound.text = location
+        if (textLabel.text!.rangeOfString("weather") != nil) {
+            
+            if myLocation != nil {
+                // get weather information for location
+                print("weather woohoo!")
+                textFound.text = "weather"
             } else {
-                print("I'm not sure what you're looking for. There seems to be a problem connecting to Suite Port. Please try asking take me to mars or what is the weather")
+                let utterance = "We are unable to determine your location, try going to a new location"
+                sayThis(utterance)
+                textFound.text = "weather not found"
             }
+        } else if ((textLabel.text!.rangeOfString("location") != nil) || (textLabel.text!.rangeOfString("where") != nil)) {
+            if myLocation != nil {
+                textFound.text = "you are currently at \(myLocation!)"
+            } else {
+                let utterance = "We are unable to determine your location, try going to a new location"
+                sayThis(utterance)
+                textFound.text = "location not found"
+            }
+        } else {
+                
+                for location in locatonList {
+                    if (textLabel.text!.rangeOfString("\(location)") != nil) {
+                        print("\(location)")
+                        textFound.text = location
+                        myLocation = location
+                        let utterance = "Taking you to \(location)"
+                        sayThis(utterance)
+                    } else {
+                        print("I'm not sure what you're looking for. There seems to be a problem connecting to Suite Port. Please try asking take me to mars or what is the weather")
+                    }
+                }
+            }
+            
+            ////TODO - Change Icon to flashing
+            recordButton.setTitle("Listen", forState: .Normal)
+            //print(result)
         }
         
-        ////TODO - Change Icon to flashing
-        recordButton.setTitle("Listen", forState: .Normal)
-        //print(result)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        // Speech boilerplate
+        // Called before speaking an utterance
+        func speechSynthesizer(synthesizer: AVSpeechSynthesizer, didStartSpeechUtterance utterance: AVSpeechUtterance) {
+            print("About to say '\(utterance.speechString)'");
+        }
+        
+        // Called when the synthesizer is finished speaking the utterance
+        func speechSynthesizer(synthesizer: AVSpeechSynthesizer, didFinishSpeechUtterance utterance: AVSpeechUtterance) {
+            print("Finished saying '\(utterance.speechString)");
+        }
+        
+        // This method is called before speaking each word in the utterance.
+        func speechSynthesizer(synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+            let startIndex = utterance.speechString.startIndex.advancedBy(characterRange.location)
+            let endIndex = startIndex.advancedBy(characterRange.length)
+            print("Will speak the word '\(utterance.speechString.substringWithRange(startIndex..<endIndex))'");
+        }
+        
+        func sayThis(utterance: String) {
+            let speechUtterance = AVSpeechUtterance(string: utterance)
+            speechUtterance.voice = self.speechVoice
+            speechUtterance.rate = 0.5
+            speechUtterance.volume = 0.5
+            speechUtterance.preUtteranceDelay = 0.0
+            speechUtterance.postUtteranceDelay = 0.0
+            speechSynthesizer.speakUtterance(speechUtterance)
+        }
 }
 
