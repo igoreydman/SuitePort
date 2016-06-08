d354e3e3107ae73ef22e303771fcab191365ccc5
Igor Eydman <igoreydman@gmail.com>
2016-06-08 13:54:28 -0400

    Replaced webView with SceneKit and CoreMotion

--- SuitePort/MainViewController.swift
+++ SuitePort/MainViewController.swift
@@ -11,6 +11,8 @@ import Foundation
 import AVFoundation
 import SpeechKit
 import WatchConnectivity
+import CoreMotion
+import SceneKit
 
 // Enables Apple Watch funcionality for iOS 9 and up
 @available(iOS 9.0, *)
@@ -30,11 +32,17 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
     // WatchKit setup
     let watchSession = WCSession.defaultSession()
     
-    @IBOutlet weak var webView: UIWebView!
+    // Scene and motion setup
+    let cameraNode = SCNNode()
+    let motionManager = CMMotionManager()
+    
+    // Removed Outlet for webView
+    @IBOutlet weak var sceneView: SCNView!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     
+    // MARK: Listen tapped
     @IBAction func recordTapped(sender: AnyObject) {
         recordButton.setTitle("Listening", forState: .Normal)
         let session = SKSession(URL: NSURL(string: "nmsps://NMDPTRIAL_igoratsuiteport_gmail_com20160428150234@sslsandbox.nmdp.nuancemobility.net:443"), appToken: "85f54358cbcdbfd0c4e508eb7f11d9accc11e7a1ddfedeb4fa02a6673eb1cbc3a60e1b71dbda419fc0498b3f5b64eb2776d3c853d1a707cba9ad32e75efeaa04")
@@ -44,19 +52,33 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
                                   delegate: self)
     }
     
+    // MARK: viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        /* Removing webView
         // Web setup
-        let url = NSURL(string: "http://suiteport.mybluemix.net/")
+        let url = NSURL(string: "http://SuitePortDemo.mybluemix.net/")
         let request = NSURLRequest(URL: url!)
         
         webView.addSubview(locationLabel)
         webView.addSubview(tempLabel)
         webView.addSubview(recordButton)
         webView.loadRequest(request)
+        */
         
-        //initiate session
+        // Setting initial teleportLocation to Earth
+        locationCheck("earth")
+        
+        // Layer buttons and labels over sceneView
+        sceneView.addSubview(locationLabel)
+        sceneView.addSubview(tempLabel)
+        sceneView.addSubview(recordButton)
+        
+        // Initiate scene and gesture control
+        sceneSetup(teleportLocation)
+        motionSetup()
+        
+        // Initiate Watch communication session
         initWCSession()
     }
     
@@ -75,6 +97,65 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
         sayThis(" ")
     }
     
+    // MARK: Scene Setup
+    func sceneSetup(teleportLocation: String!){
+        
+        // Load assets
+        guard let imagePath = NSBundle.mainBundle().pathForResource(teleportLocation, ofType: "jpg") else {
+            fatalError("Failed to find path for panaromic file.")
+        }
+        guard let image = UIImage(contentsOfFile:imagePath) else {
+            fatalError("Failed to load panoramic image")
+        }
+        
+        // Create Scene
+        let scene = SCNScene()
+        sceneView.scene = scene
+        sceneView.showsStatistics = true
+        sceneView.allowsCameraControl = true
+        
+        // TODO: Add node calibration to prevent image from starting upside-down
+        // Create a node with a sphere and the image as a texture
+        let sphere = SCNSphere(radius: 20.0)
+        sphere.firstMaterial!.doubleSided = true
+        sphere.firstMaterial!.diffuse.contents = image
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3Make(0,0,0)
+        scene.rootNode.addChildNode(sphereNode)
+        
+        // Placing camera inside the sphere
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 0, 0)
+        scene.rootNode.addChildNode(cameraNode)
+    }
+    
+    // MARK: Motion Control Setup
+    func motionSetup(){
+        
+        // Checks if motion gestures are available
+        guard motionManager.deviceMotionAvailable else {
+            fatalError("Device motion is not available")
+        }
+        
+        // Motion detection setup
+        motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
+        motionManager.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue()) {
+            [weak self](data: CMDeviceMotion?, error: NSError?) in
+            
+            guard let data = data else { return }
+            
+            // *.roll is used for front to back detection
+            // *.yaw detects virtical rotation
+            // *.pitch detects horizontal movement
+            let attitude: CMAttitude = data.attitude
+            self?.cameraNode.eulerAngles = SCNVector3Make(
+                Float(attitude.roll - M_PI/2.0),
+                Float(attitude.yaw),
+                Float(attitude.pitch))
+        }
+    }
+
+    // MARK: Record tapped result
     func transaction(transaction: SKTransaction!, didReceiveRecognition recognition: SKRecognition!) {
         let result = recognition.text.lowercaseString
         matchWords(result)
@@ -87,7 +168,7 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
             if myLocation != nil {
                 
                 // Location found, get weather information for location
-                let utterance = "The average temperature on \(myLocation!) is \(temperature!) degrees kelvin"
+                let utterance = "The effective temperature on \(myLocation!) is \(temperature!) degrees kelvin"
                 sayThis(utterance)
             } else {
                 
@@ -100,7 +181,7 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
             // User wants to know location
             if myLocation != nil {
                 
-                // Location found, where am i?
+                // Location found, where am I?
                 let utterance = "you are currently at \(myLocation!)"
                 sayThis(utterance)
             } else {
@@ -120,23 +201,20 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
                     locationCheck(location)
                     
                     // Change location
+                    /* Removing method to teleport with webView
                     changeLocation(teleportLocation!)
+                    */
+                    sceneSetup(teleportLocation)
                     let utterance = "Taking you to \(myLocation!)"
                     sayThis(utterance)
                     
                     // Update Temperature information
-                    tempLabel.text = "\(temperature!) K"
-                    
-                    //                } else if (result.rangeOfString("\(location)") == nil) {
-                    //
-                    //                    // Location is not on list or they asked for something that was not programmed
-                    //                    let utterance = "I'm not sure what you're looking for. There seems to be a problem connecting to Suite Port. Please try asking take me to mars or what is the weather"
-                    //                    sayThis(utterance)
+                    tempLabel.text = "Effective Temp: \(temperature!) K"
                 }
             }
         }
         
-        ////TODO - Change Icon to flashing
+        // TODO: Change Icon to flashing
         recordButton.setTitle("Listen", forState: .Normal)
     }
     
@@ -145,7 +223,7 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
         // Dispose of any resources that can be recreated.
     }
     
-    // Speech boilerplate
+    // MARK: Speech Setup
     // Called before speaking an utterance
     func speechSynthesizer(synthesizer: AVSpeechSynthesizer, didStartSpeechUtterance utterance: AVSpeechUtterance) {
         print("About to say '\(utterance.speechString)'");
@@ -176,10 +254,11 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
         speechSynthesizer.speakUtterance(speechUtterance)
     }
     
+    /* Removing web calls
     func changeLocation(teleportLocation: String) {
         
         // Setup the session to make REST GET call
-        let postEndpoint: String = "https://suiteport.mybluemix.net/alexa/request"
+        let postEndpoint: String = "SuitePortDemo.mybluemix.net/alexa/request"
         let session = NSURLSession.sharedSession()
         let url = NSURL(string: postEndpoint)!
         let postParams : [String : AnyObject] = ["location" : "\(teleportLocation)"]
@@ -213,6 +292,7 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
             }
         }).resume()
     }
+    */
     
     func locationCheck(location: String) {
         
@@ -228,7 +308,7 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
             break
         case "mars":
             myLocation = "mars"
-            let marsArray = ["mars", "mars 1", "mars 2", "mars 3"]
+            let marsArray = ["mars-1", "mars-2", "mars-3"]
             teleportLocation = marsArray[Int(arc4random_uniform(UInt32(marsArray.count)))]
             temperature = "212"
             locationLabel.text = "Mars"
@@ -237,7 +317,7 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
             break
         case "moon":
             myLocation = "the moon"
-            teleportLocation = myLocation
+            teleportLocation = "moon"
             temperature = "268"
             locationLabel.text = "The Moon"
             locationLabel.textColor = UIColor.whiteColor()
@@ -249,14 +329,14 @@ class MainViewController: UIViewController, AVSpeechSynthesizerDelegate, WCSessi
         }
     }
     
-    // Initiate Apple Watch session
+    // TODO: Apple Watch Session
     func initWCSession() {
         watchSession.delegate = self
         watchSession.activateSession()
     }
     
     func session(session: WCSession, didReceiveMessage message: [String : AnyObject]) {
-        
+        // watch session
     }
 }
 
